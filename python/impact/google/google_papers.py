2fcaa53d2cd1abcd9ab42f01bc9c61bea57c0d1f
Rodrigo Dorantes-Gilardi <rodgdor@gmail.com>
2024-06-17 13:40:27 -0400

    Update google papers

--- python/impact/google/google_papers.py
+++ python/impact/google/google_papers.py
@@ -5,8 +5,8 @@ import json
 cohorts = pd.read_csv('../../../data/expansion/cohorts.csv')
 cohort_names = cohorts['cohort_name_lower'].to_list()
 
-API_KEY = "AIzaSyBsUR5AP6bpWLrwwIPid3EXaITyeG8OHug"
-SEARCH_ENGINE = "31a8c6002de144d1c"
+API_KEY = "API KEY"
+SEARCH_ENGINE = "SEARCH ENGINE ID"
 URL = "https://www.googleapis.com/customsearch/v1"
 
 params = {
